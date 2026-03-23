3ae1d998ced740fb9b354e3bf641b6fff659dd9c
Brayan <brayanmartinez1020@gmail.com>
2026-03-23 10:20:09 -0400

    Mi primer commits

---
+++ Main.bat
@@ -0,0 +1,2 @@
+cd C:\Users\root\Downloads\rocketbot_win_20251015\Rocketbot
+rocketbot.exe -start=Main -db="C:/Users/root/Downloads/Nuevo proyecto\Main\robot.db" 
---
+++ README.md
@@ -0,0 +1,1 @@
+# MainNuevo proyecto de Rocketbot Studio
